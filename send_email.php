a9e9a34365b284ce61b283655437d258ffe7f7c5
Prafulla Kalusinh Garasia <W0797964@myscc.ca>
2023-04-25 23:01:01 -0400

    update contact page

--- send_email.php
+++ send_email.php
@@ -1,26 +1,26 @@
 <?php
-$to_email = "prafullagarasia@gmail.com";
+if(isset($_POST['submit'])){
+    $to = "prafullagarasia@gmail.com"; //  email address
+    $subject = $_POST['subject'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['msg'];
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Get the form data
-  $name = strip_tags($_POST["name"]);
-  $email = strip_tags($_POST["email"]);
-  $subject = strip_tags($_POST["subject"]);
-  $message = strip_tags($_POST["msg"]);
+    $body = "<html><body>";
+    $body .= "<h2>Contact Form Submission</h2>";
+    $body .= "<p><strong>Name:</strong> $name</p>";
+    $body .= "<p><strong>Email:</strong> $email</p>";
+    $body .= "<p><strong>Message:</strong> $message</p>";
+    $body .= "</body></html>";
 
-  // Compose the email message
-  $message_body = "Name: $name\nEmail: $email\n\n$message";
-
-  // Set the email headers
-  $headers = "From: $name <$email>\r\nReply-To: $email\r\n";
-
-  // Send the email
-  if (mail($to_email, $subject, $message_body, $headers)) {
-    // Display a success message to the user
-    echo "<p>Thank you for your message. We will get back to you as soon as possible.</p>";
-  } else {
-    // Display an error message to the user
-    echo "<p>There was an error sending your message. Please try again later.</p>";
-  }
+    if(mail($to, $subject, $body, $headers)){
+        echo "Thank you for contacting us!";
+    } else{
+        echo "Sorry, there was an error sending your message. Please try again later.";
+    }
 }
 ?>
